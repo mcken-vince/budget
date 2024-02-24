0b07c7c20c0d8425386ed27f1e104a3aa98df74d
Vincent <mcken.vince@gmail.com>
2024-02-23 20:09:10 -0700

    Setup AuthModule and jwt strategy

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransactionModule } from './transactions/transaction.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../modules/auth/auth.module';
 
 @Module({
-  imports: [TransactionModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, TransactionModule],
+  providers: [AppService],
 })
 export class AppModule {}

ade9a5be8b53a4fc37112cd10247639358971a63
Vincent <mcken.vince@gmail.com>
2024-02-23 20:06:46 -0700

    Setup database config

--- api/src/database/database.providers.ts
+++ api/src/database/database.providers.ts
@@ -1,19 +1,28 @@
 import { Sequelize } from 'sequelize-typescript';
+import { databaseConfig } from './database.config';
+import { User, Budget, Transaction, Category } from '../models';
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'postgres',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'password',
-        database: 'nest',
-      });
-      sequelize.addModels([]);
-      await sequelize.sync();
+      let config;
+      switch (process.env.NODE_ENV) {
+        case 'development':
+          config = databaseConfig.development;
+          break;
+        case 'test':
+          config = databaseConfig.test;
+          break;
+        case 'production':
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      const sequelize = new Sequelize(config);
+      sequelize.addModels([User, Budget, Transaction, Category]);
+      // await sequelize.sync({ force: true });
       return sequelize;
     },
   },

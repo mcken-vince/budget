a2ce34b6e04886d56fad4bb5ef694d0664954ae3
Vincent <mcken.vince@gmail.com>
2024-02-24 17:01:19 -0700

    Use ValidatePipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,12 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ValidateInputPipe } from './pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 8000;
+  app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`

d88ef039c5358e262ece964841a3e5fcd9991149
Vincent <mcken.vince@gmail.com>
2024-02-28 20:34:59 -0700

    EnableCors on api, use global LoggingInterceptor

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,12 +8,20 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 8000;
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(port);
   Logger.log(

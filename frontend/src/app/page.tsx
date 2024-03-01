470a86d204522f4f2819928a835c9626f413e61f
Vincent <mcken.vince@gmail.com>
2024-03-01 11:35:58 -0700

    Display session on index page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default async function Index() {
-  return <h1>Index Page</h1>;
+'use client';
+
+import { useSession } from 'next-auth/react';
+
+export default function Index() {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <h3>Session: {JSON.stringify(session)}</h3>
+    </div>
+  );
 }

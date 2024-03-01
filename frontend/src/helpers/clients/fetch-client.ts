b9fdc526db625bc4f01b55e9daf064b5819ec241
Vincent <mcken.vince@gmail.com>
2024-02-29 20:15:05 -0700

    Create apiFetch request method

---
+++ frontend/src/helpers/clients/fetch-client.ts
@@ -0,0 +1,26 @@
+export const apiFetch = async (endpoint: string, options?: ApiFetchOptions) => {
+  try {
+    const res = await fetch(
+      `http://localhost:8000/api/${
+        endpoint[0] === '/' ? endpoint.slice(1) : endpoint
+      }`,
+      {
+        method: options?.method || 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: options?.data ? JSON.stringify(options.data) : null,
+      }
+    );
+    const response = await res.json();
+    console.log({ response });
+    return response;
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+export interface ApiFetchOptions {
+  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  data?: Record<string, any>;
+}

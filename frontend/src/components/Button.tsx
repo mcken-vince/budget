989db1a5fd85f366246e5ca1058030bad9351956
Vincent <mcken.vince@gmail.com>
2024-03-01 10:41:57 -0700

    Convert Button and Input components to use HyperUI

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,12 +1,21 @@
 export type ButtonProps = {
   children: any;
   onClick?: (e: any) => void;
-  className?: string;
+  variant?: 'solid' | 'border';
+  type?: 'button' | 'reset' | 'submit';
 };
 
-export function Button({ children, onClick, className }: ButtonProps) {
+export function Button({ children, onClick, variant, type }: ButtonProps) {
   return (
-    <button className={'' + className} onClick={onClick}>
+    <button
+      type={type}
+      className={`inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium ${
+        variant === 'border'
+          ? 'text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500'
+          : 'bg-indigo-600 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500'
+      }`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -25,21 +25,19 @@ export const Input = ({
 }: InputProps) => {
   return (
     <div>
-      {label && (
-        <label htmlFor={name} className="sr-only">
-          {label}
-        </label>
-      )}
+      <label htmlFor={name} className="block text-xs font-medium text-gray-700">
+        {label}
+      </label>
+
       <input
-        id={name}
-        name={name}
         type={type}
-        autoComplete={autoComplete}
-        required={required}
+        id={name}
+        placeholder={placeholder}
+        className="mt-1 p-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
         value={value}
         onChange={onChange}
-        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-        placeholder={placeholder}
+        required={required}
+        autoComplete={autoComplete}
       />
     </div>
   );

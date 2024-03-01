49bfc375bd2c7c5e4c75752819668f75e4a252c4
Vincent <mcken.vince@gmail.com>
2024-03-01 11:35:26 -0700

    Use hyperui in TopNav

--- frontend/src/components/NavLink.tsx
+++ frontend/src/components/NavLink.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function NavLink({ href, label }: { href: string; label: string }) {
   return (
-    <Link href={href} className="text-white hover:text-gray-300">
+    <Link href={href} className="text-gray-500 hover:text-gray-300">
       {label}
     </Link>
   );

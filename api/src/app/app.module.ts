b135923164b4ef1956f4c6fa8339b397043396a2
Vincent <mcken.vince@gmail.com>
2024-03-02 16:17:00 -0700

    Add TransactionModule to app.module imports

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../core/database/database.module';
 import { AuthModule } from '../modules/auth/auth.module';
+import { TransactionModule } from '../modules/transactions/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AuthModule,
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],

159c2ecaf4691788ba022fc469b23046eb41d580
Vincent <mcken.vince@gmail.com>
2024-03-04 16:03:05 -0700

    Use real links in TopNav

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 export const TopNav = () => {
   const { data: session, status } = useSession();
   const authenticated = status === 'authenticated';
+
   return (
     <header className="bg-white">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
@@ -30,27 +31,10 @@ export const TopNav = () => {
             {authenticated && (
               <ul className="flex items-center gap-6 text-sm">
                 <li>
-                  <NavLink href="#" label="About" />
+                  <NavLink href="/" label="Home" />
                 </li>
-
                 <li>
-                  <NavLink href="#" label="Careers" />
-                </li>
-
-                <li>
-                  <NavLink href="#" label="History" />
-                </li>
-
-                <li>
-                  <NavLink href="#" label="Services" />
-                </li>
-
-                <li>
-                  <NavLink href="#" label="Projects" />
-                </li>
-
-                <li>
-                  <NavLink href="#" label="Blog" />
+                  <NavLink href="/transactions" label="Transactions" />
                 </li>
               </ul>
             )}
@@ -59,19 +43,19 @@ export const TopNav = () => {
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               {authenticated ? (
-                <a
-                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                <button
+                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 "
                   onClick={() => signOut()}
                 >
                   Logout
-                </a>
+                </button>
               ) : (
-                <a
+                <Link
                   className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
                   href="/login"
                 >
                   Login
-                </a>
+                </Link>
               )}
 
               {!authenticated && (

be93199626f8b6fdced72eb1d04e7d96d9cff928
Vincent <mcken.vince@gmail.com>
2024-03-04 20:16:58 -0700

    Changes to Dto class validators

--- api/src/core/dto/transaction.dto.ts
+++ api/src/core/dto/transaction.dto.ts
@@ -1,23 +1,39 @@
-import { IsNotEmpty, IsString, IsDate, IsNumber } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsOptional,
+  MaxLength,
+  Min,
+  IsDateString,
+} from 'class-validator';
 
 export class TransactionDto {
-  @IsNotEmpty()
-  readonly idUser: number;
-
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
   readonly idBudget?: number;
 
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
   readonly idCategory?: number;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Name is required' })
   @IsString()
+  @MaxLength(255, { message: 'Limit is 255 characters' })
   readonly name: string;
 
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000, { message: 'Limit is 1000 characters' })
   readonly description?: string;
 
-  @IsDate()
+  @IsNotEmpty({ message: 'Date is required' })
+  @IsDateString()
   readonly date: Date;
 
-  @IsNotEmpty()
-  @IsNumber()
+  @IsNotEmpty({ message: 'Amount is required' })
+  @IsNumber({ allowInfinity: false, maxDecimalPlaces: 2 })
   amount: number;
 }
--- api/src/core/dto/user.dto.ts
+++ api/src/core/dto/user.dto.ts
@@ -1,10 +1,20 @@
-import { IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+import {
+  IsNotEmpty,
+  MinLength,
+  IsEmail,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class UserDto {
   @IsNotEmpty()
+  @IsString()
+  @MaxLength(255, { message: 'Limit is 255 characters' })
   readonly firstName: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MaxLength(255, { message: 'Limit is 255 characters' })
   readonly lastName: string;
 
   @IsNotEmpty()
@@ -12,6 +22,7 @@ export class UserDto {
   readonly email: string;
 
   @IsNotEmpty()
+  @IsString()
   @MinLength(6)
   readonly password: string;
 }

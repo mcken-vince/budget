f403e3bc2b5a5c4fe7df834faa5f60ee43681925
Vincent <mcken.vince@gmail.com>
2024-03-04 20:17:32 -0700

    Add logs to ValidateInputPipe and handle regular error vs BadRequestException from ClassValidator

--- api/src/core/pipes/validate.pipe.ts
+++ api/src/core/pipes/validate.pipe.ts
@@ -12,13 +12,20 @@ export class ValidateInputPipe extends ValidationPipe {
     try {
       return await super.transform(value, metadata);
     } catch (e) {
+      console.log({ ValidateInputPipeError: e });
       if (e instanceof BadRequestException) {
-        throw new UnprocessableEntityException(this.handleError(e.message));
+        throw new UnprocessableEntityException(
+          this.handleError(e?.['response']?.message || e.message)
+        );
       }
     }
   }
 
   private handleError(errors) {
-    return errors.map((error) => error.constraints);
+    console.log({ HandleErrorMessage: errors });
+    if (Array.isArray(errors)) {
+      return errors.map((error) => error.constraints);
+    }
+    return errors;
   }
 }

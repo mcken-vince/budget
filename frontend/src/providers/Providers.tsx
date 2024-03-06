31c8083c5af67db6f5b12a52388e23cb77817f60
Vincent <mcken.vince@gmail.com>
2024-03-06 10:31:36 -0700

    Add PopupProvider to Providers wrapper

--- frontend/src/providers/Providers.tsx
+++ frontend/src/providers/Providers.tsx
@@ -2,11 +2,16 @@
 
 import { ReactNode } from 'react';
 import AuthContext from './AuthContext';
+import { PopupProvider } from './PopupProvider';
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
-  return <AuthContext>{children}</AuthContext>;
+  return (
+    <AuthContext>
+      <PopupProvider>{children}</PopupProvider>
+    </AuthContext>
+  );
 }

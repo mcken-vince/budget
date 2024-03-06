6b9179512fd88701eaafc9220b04249cccbd46b4
Vincent <mcken.vince@gmail.com>
2024-03-05 21:00:33 -0700

    Create Select component

---
+++ frontend/src/components/Inputs/Select.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent } from 'react';
+import { InputErrorMessage } from './InputErrorMessage';
+
+export interface SelectProps {
+  name: string;
+  label?: string;
+  value: any;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  placeholder?: string;
+  error?: string;
+  options: { value: any; label: string }[];
+}
+
+export const Select = ({
+  name,
+  label,
+  value,
+  onChange,
+  options,
+  placeholder,
+  error,
+}: SelectProps) => {
+  return (
+    <div>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-900">
+        {label}
+      </label>
+
+      <select
+        name={name}
+        id={name}
+        className="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+        onChange={onChange}
+        value={value}
+      >
+        <option value="">{placeholder ?? 'Please select'}</option>
+        {options?.map((option, idx) => (
+          <option key={`select-${name}-option-${idx}`} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <InputErrorMessage error={error} />
+    </div>
+  );
+};

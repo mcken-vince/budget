67a859c5d6de5291c7d5252b04f4060c83477d8d
mcken-vince <mcken.vince@gmail.com>
2024-03-07 15:07:59 -0700

    Upgrade Modal component

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,4 +1,6 @@
+import { XCircleIcon } from '@heroicons/react/16/solid';
 import { ReactElement } from 'react';
+import { IconButton } from './Buttons/IconButton';
 
 export interface ModalProps {
   show: boolean;
@@ -7,6 +9,7 @@ export interface ModalProps {
   onSubmit?: () => Promise<void>;
   children: ReactElement;
   closeButtonText?: string;
+  preventCloseOnClickOutside?: boolean;
 }
 
 export const Modal = ({
@@ -16,14 +19,24 @@ export const Modal = ({
   onSubmit,
   closeButtonText,
   children,
+  preventCloseOnClickOutside,
 }: ModalProps) => {
   if (!show) return null;
   return (
-    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+    <div
+      className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-slate-800/50"
+      onClick={preventCloseOnClickOutside ? undefined : onClose}
+    >
+      <div
+        className="relative w-auto my-6 mx-auto max-w-3xl"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+          <div className="flex items-middle justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
             <h3 className="text-3xl font=semibold">{title}</h3>
+            <IconButton onClick={onClose}>
+              <XCircleIcon className="h-5 w-5 self-center" />
+            </IconButton>
           </div>
           <div className="relative p-6 flex-auto">{children}</div>
           <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">

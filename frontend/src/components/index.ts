ec507ce89866db2fa318ca32db221ba7d7df0ada
mcken-vince <mcken.vince@gmail.com>
2024-03-11 20:13:34 -0600

    Export components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -5,6 +5,7 @@ export * from './Inputs/Input';
 export * from './Inputs/InputErrorMessage';
 export * from './SectionTitle';
 export * from './SectionHeader';
+export * from './Progress';
 
 // Menus
 export * from './DropdownMenu';
@@ -24,8 +25,12 @@ export * from './Login/SignUpForm';
 // Transactions
 export * from './Transactions/TransactionForm';
 export * from './Transactions/TransactionsTable';
+export * from './Transactions/TransactionsByMonth';
 
 // Category
 export * from './Category/CategoryForm';
 export * from './Category/CategorySelector';
 export * from './Category/ManageCategoriesTable';
+
+// Budgets
+export * from './Budgets/BudgetOverview';

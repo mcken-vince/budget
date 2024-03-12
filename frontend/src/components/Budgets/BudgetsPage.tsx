bf6711e1c3249bcc604f21db70a45a1a1e62f498
mcken-vince <mcken.vince@gmail.com>
2024-03-12 14:38:44 -0600

    Create BudgetsPage

---
+++ frontend/src/components/Budgets/BudgetsPage.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import { BudgetForm } from './BudgetForm';
+import { useSession } from 'next-auth/react';
+import { apiFetch } from '@helpers/clients';
+import { SectionHeader } from '..';
+import { BudgetsTable } from './BudgetsTable';
+import { addToState } from '@helpers/state.helpers';
+
+export const BudgetsPage = () => {
+  const { data: session } = useSession();
+  const [categories, setCategories] = useState<any[]>([]);
+  const [budgets, setBudgets] = useState<any[]>([]);
+
+  const addBudget = useMemo(() => addToState(setBudgets), []);
+
+  useEffect(() => {
+    if (session?.auth_token) {
+      (async function fetchCategories() {
+        const response = await apiFetch('category', {
+          token: session?.auth_token,
+        });
+        if (Array.isArray(response)) setCategories(response);
+      })();
+      (async function fetchBudgets() {
+        const response = await apiFetch('budget', {
+          token: session?.auth_token,
+        });
+        if (Array.isArray(response)) setBudgets(response);
+      })();
+    }
+  }, [session?.auth_token]);
+
+  return (
+    <div>
+      <SectionHeader title="Budgets">
+        <BudgetForm addBudget={addBudget} categories={categories} />
+      </SectionHeader>
+      <BudgetsTable
+        budgets={budgets}
+        categories={categories}
+        updateBudget={() => {
+          return;
+        }}
+      />
+    </div>
+  );
+};

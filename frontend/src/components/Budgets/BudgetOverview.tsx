1726eaf617bf5fc0bc919306abc31fdcb0b5603a
mcken-vince <mcken.vince@gmail.com>
2024-03-12 14:39:00 -0600

    Changes to BudgetOverview

--- frontend/src/components/Budgets/BudgetOverview.tsx
+++ frontend/src/components/Budgets/BudgetOverview.tsx
@@ -1,36 +1,95 @@
 'use client';
 
 import { ProgressBar } from '@components/Progress';
+import { formatDate } from '@helpers/date.helpers';
+import { useMemo, useState } from 'react';
 
 export interface BudgetOverviewProps {
-  category: any;
+  budget: any;
   total?: number;
   budgetAmount?: number;
   hideBudgetAmount?: boolean;
+  transactions?: any[];
   onClick?: () => void;
+  uncategorized?: boolean;
 }
 
 export const BudgetOverview = ({
-  category,
-  total = 0,
+  budget,
   budgetAmount = 0,
   hideBudgetAmount,
+  transactions = [],
   onClick,
 }: BudgetOverviewProps) => {
-  const percentage = (total / budgetAmount) * 100 || 0;
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const spend = useMemo(
+    () =>
+      transactions.reduce((acc, transaction) => acc + transaction.amount, 0),
+    [transactions]
+  );
+  const percentage = (spend / budgetAmount) * 100 || 0;
   return (
     <div
-      className="hover:bg-sky-100 hover:cursor-pointer p-2"
-      onClick={onClick}
+      className="hover:bg-slate-300 hover:cursor-pointer p-2"
+      onClick={() => {
+        if (onClick) onClick();
+        if (transactions.length) setDrawerOpen((prev) => !prev);
+      }}
     >
       <div className="flex justify-between mb-1">
-        <h3>{category.name}</h3>
+        <h3>{budget?.name}</h3>
         <h3>
-          ${total.toFixed(2)}
-          {hideBudgetAmount ? null : ` of ${budgetAmount}`}
+          ${spend.toFixed(2)}
+          {hideBudgetAmount ? null : ` of $${budgetAmount.toFixed(2)}`}
         </h3>
       </div>
-      <ProgressBar progress={percentage} />
+      <ProgressBar
+        progress={percentage}
+        uncategorizedBudgetOverride={hideBudgetAmount}
+      />
+      <div
+        className={`overflow-hidden transition-all duration-500 ${
+          drawerOpen ? 'max-h-96' : 'max-h-0'
+        }`}
+      >
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y-2 divide-slate-200 bg-white text-sm">
+            <thead className="text-left">
+              <tr>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-slate-900">
+                  Name
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-slate-900">
+                  Date
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-slate-900 text-right">
+                  Amount
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200">
+              {transactions?.map((transaction, idx) => (
+                <tr
+                  key={`budget-${budget?.name}-transaction-${
+                    transaction?.id || idx
+                  }`}
+                >
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    {transaction?.name}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                    {formatDate(transaction?.date)}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-right">
+                    ${transaction?.amount.toFixed(2)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };

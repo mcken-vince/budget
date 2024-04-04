d69dce12c888d6faaae3019030139d2880aeec45
mcken-vince <mcken.vince@gmail.com>
2024-04-04 16:54:54 -0600

    Change modal title size

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -59,7 +59,7 @@ export const Modal = ({
       >
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
           <div className="flex items-middle justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-            <h3 className="text-3xl font=semibold">{title}</h3>
+            <h3 className="text-2xl font=semibold">{title}</h3>
             <IconButton onClick={onClose}>
               <XCircleIcon className="h-5 w-5 self-center" />
             </IconButton>
